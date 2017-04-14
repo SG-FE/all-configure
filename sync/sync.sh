b6bac4a95a00c8f31fe7448d670c7242b148b229
maoguoyou <448236822@qq.com>
2017-04-14 18:43:12 +0800

    test

--- sync/sync.sh
+++ sync/sync.sh
@@ -1,3 +1,89 @@
+<<<<<<< HEAD
+#! /usr/bin/env bash
+
+type=$1
+
+if [ -z "${type}"  ]; then
+  echo "need a type"
+  exit 1
+fi
+
+case "$type" in
+  web)
+    syname="webhuohuo"
+    ;;
+  webapp)
+    syname="webapphuohuo"
+    ;;
+  cms)
+    syname="cmshuohuo"
+    ;;
+  askweb)
+    syname="askwebdevelop"
+    ;;
+  askwebapp)
+    syname="askwebappdevelop"
+    ;;
+  sxyweb)
+    syname="sxywebhuohuo"
+    ;;
+  sxywebapp)
+    syname="sxywebappdevelop"
+    ;;
+  askwebcms)
+    syname="askcmsdevelop"
+    ;;
+  restful)
+    syname="servicehuohuo"
+    ;;
+  ucweb)
+    syname="ucwebhuohuo"
+    ;;
+  ucapp)
+    syname="ucapphuohuo"
+    ;;
+esac
+
+if [ -z "$syname" ]; then
+  echo "type must be web, webapp, cms"
+  exit 1
+fi
+
+pid=/tmp/unicorn-${type}.pid
+
+# stop service
+if [ -f ${pid} ]; then
+  echo "shutdown servic"
+  uwsgi --stop ${pid}
+fi
+
+echo "removing files"
+rm -rf ./${type}/server
+
+rsync -avzP --delete --exclude="logs/*"  --password-file=/etc/rsync.pas work@120.25.103.109::${syname} ./${type}
+
+if [ $? != 0 ]; then
+  echo "updating code failed"
+  exit 1;
+fi
+
+# change user to vagrant
+if [ $type = "restful" ]; then
+  cd ${type}
+else
+  cd ${type}/server
+fi
+
+# start service
+ini=./uwsgi_vagrant.ini
+if [ -f ${ini}  ]; then
+  echo "starting service..."
+  uwsgi ${ini}
+else
+  echo "require uwsgi_vagrant.ini"
+  exit 1
+fi
+=======
 #! /usr/bin/env bash
 
 type=$1
@@ -85,3 +171,4 @@ else
   echo "require uwsgi_vagrant.ini"
   exit 1
 fi
+>>>>>>> b86741313f8fd88c3ed861455b8c86bd9dcac34e
